b5056181267074fa9f1b1c546ab287080f0bb05e
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2019-07-10 14:10:22 -0700

    working version

--- mad-libs.py
+++ mad-libs.py
@@ -3,14 +3,14 @@ import os
 def cls():
 	os.system('clear')
 
-def ask(phrase : str) -> str:
+def ask(phrase, n='??'):
+#    print('Question  of XX')
     print('>>>Answer the question then press enter<<<\n')
     print(phrase)
     response = input('>> ')
     input('\nPlease press enter and pass the computer...')
     cls()
     return response.lower()
-
 
 ### BEGIN PROGRAM ###
 cls()
@@ -32,15 +32,57 @@ else:
     season = "summer"
 
 #Year
-year = ask('Please enter a year')
+year1 = ask('Please enter a year')
 
 #Age
-age = ask('Please enter a number between 1 and 999')
+age1 = ask('Please enter a number between 1 and 999')
 
 #Noun 1
-noun1 = ask ('
-story_string = ""
-story_string += noun1+" was "+verb1+" there way to the "+place1+" with their "+noun2+'. But suddenly, a wild '+noun3+' approaches! Your '+noun2+' '+verb2+' '+adverb1+'.'
+noun1 = ask ('Please enter a noun')
+
+#Name 1 
+name1 = ask('Please enter a name')
+
+#Verb 1 
+verb1 = ask("""Enter a verb (ending in 'ing')""")
+
+#Street name 1 
+street1 = ask('Enter the name of a street')
+
+#Adverb 1 
+adverb1 = ask("""Please enter an adverb (BTW adverbs end in '-ly')""")
+
+#Adjective 1 
+adjective1 = ask('Please enter an adjective')
+
+#Noun 2
+noun2 = ask('Please enter a noun')
+
+#Place 1 
+place1 = ask('Enter the name of a place')
+
+#Body part 1 
+body_part1 = ask('Enter the name of a body part')
+
+#Verb 2 
+verb2 = ask("""Please enter a verb (ending in 'ing')""")
+
+#Verb 3 
+verb3 = ask('Please enter a verb (Past tense)')
+
+#Adjective 2
+adjective2 = ask('Please enter an adjective')
+
+#Verb 4 
+verb4 = ask("""Please enter a verb (ending in 'ing')""")
+
+#Name 2
+name2 = ask('Enter a name')
+
+#Animal 1 
+animal1 = ask('Enter a type of animal')
+
+story_string = 'In the '+adjective1+' '+season+' of '+year1+'. There was a '+age1+' year old '+noun1+', and it was named '+name1+'. It was '+verb1+' down '+street1+' street when '+adverb1+' a '+adjective2+' '+noun1+' popped out of the '+noun2+'. Scared out of his '+body_part1+', the '+noun1+' '+verb3+'. The rain was '+adjective2+' and the clouds closed in. All of a sudden, the clouds began to '+verb4+' and '+name2+' came down from the skies in the form of a '+animal1+'.'
+
 
 print(story_string)
-
