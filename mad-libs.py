0f87ea32488b3d1184eeb4e36d506c1830071d6a
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2019-07-16 13:33:54 -0700

    removed old file

--- mad-libs.py
+++ mad-libs.py
@@ -1,89 +1,88 @@
-import os
+#import tkinter for graphics
+from tkinter import *
 
+#list for questions and answers
+q_area51 = ['Enter an adjective related to weather...', 'Enter a number between 100 and 300...', 'Enter a number between 10 and 20...', 'Enter the name of a song...', 'Enter an adjective relating to birthday parties...', 'Enter an adjective relating to evil...', 'Enter the name of the person physically closest to you...', 'Enter a number between 1000 and 4000...', 'Enter an adjective that reminds you of prison...', 'Fortnite kids or anti-vax kids...', 'Enter an adjective related to anger...']
+questions = q_area51
+answers = []
 
-def cls():
-	os.system('clear')
+#function for when submit button is clicked
+def click(self):
+    if(lbl1.winfo_ismapped()):
+            begin(self)
+            return 0
 
-def ask(phrase):
-    #print('Question '+n+' of 20')
-    print('>>>Answer the question then press enter<<<\n')
-    print(phrase)
-    response = input('>> ')
-    input('\nPlease press enter and pass the computer...')
-    cls()
-    return response.lower()
+    #append answer from textbox to answers list
+    answers.append(textentry.get())
 
-### BEGIN PROGRAM ###
-cls()
+    #empty text box
+    textentry.delete(0, END)
 
-#Adjective 1
-adjective1 = ask('Please enter an adjective')
+    #add one to the question number
+    global q_num
+    q_num+=1
 
-#Season 
-season = ask("""Choose a season...\n\t1. Summer\n\t2. Autumn\n\t3. Winter\n\t4. Spring\n""")
-if season == 1:
-    season = "summer"
-elif season == 2:
-    season = "autumn"
-elif season == 3:
-    season = "winter"
-elif season == 4:
-    season = "spring"
-else: 
-    season = "summer"
+    #return string for question number and move to next question text
+    q_num_str.set('Question '+str(q_num)+' of '+str(len(questions)))
+    q_act.set(questions[q_num-1])
 
-#Year
-year1 = ask('Please enter a year')
+    if q_num == len(questions):
+        #run clear screen and show story
+        #window.withdraw()
+        #input.destroy()
+        var1 = 0
 
-#Age
-age1 = ask('Please enter a number between 1 and 999')
+#move to normal game, delete beginning and play button
+def begin(self):
+    lbl1.pack_forget()
+    lbl2.pack_forget()
 
-#Noun 1
-noun1 = ask ('Please enter a noun')
+#create tkinter window, set title, background color, and size
+window = Tk()
+window.title('Mad-Libs')
+window.configure(background='black')
+window.geometry('1000x600')
 
-#Name 1 
-name1 = ask('Please enter a name')
+#start screen with images
+img = PhotoImage(file='bg_1000x600.png')
+img2 = PhotoImage(file='begin_1000x.png')
+lbl1 = Label(window,image=img2,bg='black')
+lbl1.pack(side='bottom')
+lbl2 = Label(window,image=img,bg='black')
+lbl2.pack()
 
-#Verb 1 
-verb1 = ask("""Enter a verb (ending in 'ing')""")
+#enter button does the same thing as the submit button
+window.bind('<Return>', click)
+window.bind('<Button-1>',begin)
 
-#Street name 1 
-street1 = ask('Enter the name of a street')
+#title
+Label(window,text='Mad-Libs',bg='black',fg='red',font='Courier 50 bold').pack(pady='10')
 
-#Adverb 1 
-adverb1 = ask("""Please enter an adverb (BTW adverbs end in '-ly')""")
+#initialize question numbers and initial question
+q_act = StringVar()
+q_act.set(questions[0])
 
-#Adjective 1 
-adjective1 = ask('Please enter an adjective')
+q_num_str = StringVar()
+q_num_str.set("Question 1 of "+str(len(questions)))
+q_num = 1
 
-#Noun 2
-noun2 = ask('Please enter a noun')
+#question text
+Label(window,textvariable=q_act,bg='black',fg='red',font='Courier 20').pack(pady='10')
 
-#Place 1 
-place1 = ask('Enter the name of a place')
+#entry box for text
+input = textentry = Entry(window,width=20,bg='white',font='Courier 16')
+input.pack(pady='20')
 
-#Body part 1 
-body_part1 = ask('Enter the name of a body part')
+#question number string
+Label(window,textvariable=q_num_str,bg='black',fg='red',font='Roboto 20').pack(pady='10')
 
-#Verb 2 
-verb2 = ask("""Please enter a verb (ending in 'ing')""")
+#submit button
+Button(window,text='SUBMIT',width=10,command=click).pack()
 
-#Verb 3 
-verb3 = ask('Please enter a verb (Past tense)')
+#question number and instructions on the bottom
+Label(window, bg='black',fg='red',text='Created by: BL4Z3, h4ckerm4n_z4ch, b1g n@te, and PW3Z',font='Courier 18').pack(side='bottom',pady='10')
 
-#Adjective 2
-adjective2 = ask('Please enter an adjective')
+Label(window,bg='black',fg='white',text='Answer the question in the text box and press enter',font='Courier 15').pack(side="bottom")
 
-#Verb 4 
-verb4 = ask("""Please enter a verb (ending in 'ing')""")
-
-#Name 2
-name2 = ask('Enter a name')
-
-#Animal 1 
-animal1 = ask('Enter a type of animal')
-
-story_string = 'In the '+adjective1+' '+season+' of '+year1+'. There was a '+age1+' year old '+noun1+', and it was named '+name1+'. It was '+verb1+' down '+street1+' street when '+adverb1+' a '+adjective2+' '+noun1+' popped out of the '+noun2+'. Scared out of his '+body_part1+', the '+noun1+' '+verb3+'. The rain was '+adjective2+' and the clouds closed in. All of a sudden, the clouds began to '+verb4+' and '+name2+' came down from the skies in the form of a '+animal1+'.'
-
-
-print(story_string)
+#begin main GUI loop
+window.mainloop()
